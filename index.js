25ab145e570b4803e84be8a9dd12afcbbbc6d6b4
Alex <san91130324@gmail.com>
2017-02-20 18:38:19 +0300

    add front end

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var p=8;
 var maxElInPipeline=5;
 var ElInPipeline=0;
 var ArrayOfPipelineElement= new Array();
+var ArrayRezOfWork= new Array();
 
 function startPipeline() {
     for(var index=0; index<ArrayOfPipelineElement.length-1; index++)
@@ -56,7 +57,7 @@ function createPipeline() {
             if(workObj.index<(workObj.bitnum).toString(2).length-1)
                 workObj.rez<<=1;
             ArrayOfPipelineElement[index+1].stack.push(new PipelineObj(workObj.rez,workObj.num,++workObj.index,workObj.bitnum));
-
+            ArrayRezOfWork[index]=workObj.rez;
 
             setTimeout(ArrayOfPipelineElement[index].SumAndMove(index), 1);
         },
@@ -140,6 +141,8 @@ function createPipeline() {
 
             ElInPipeline--;
             console.log(workObj.rez);
+            ArrayRezOfWork[index]=workObj.rez;
+
             if(ElInPipeline+1>=maxElInPipeline&&ArrayOfPipelineElement[0].stack.length!=0){
                 // console.log("too march el in pipeline "+ElInPipeline);
                 setTimeout(ArrayOfPipelineElement[0].SumAndMove(0), 1);
@@ -157,31 +160,34 @@ function createPipeline() {
     }
     ArrayOfPipelineElement.push(el);
 }
-createPipeline();
-addToPipeline(1,2);
-addToPipeline(1,3);
-addToPipeline(1,4);
-addToPipeline(1,5);
-addToPipeline(1,6);
-addToPipeline(1,7);
-addToPipeline(1,8);
-addToPipeline(1,9);
-addToPipeline(1,10);
-addToPipeline(1,11);
-addToPipeline(1,12);
-addToPipeline(1,13);
-addToPipeline(1,14);
-addToPipeline(1,15);
-addToPipeline(1,16);
-addToPipeline(1,17);
-addToPipeline(1,18);
-addToPipeline(1,19);
+
+    createPipeline();
+    addToPipeline(1,2);
+    addToPipeline(1,3);
+    addToPipeline(1,4);
+    addToPipeline(1,5);
+    addToPipeline(1,6);
+    addToPipeline(1,7);
+    addToPipeline(1,8);
+    addToPipeline(1,9);
+    addToPipeline(1,10);
+    addToPipeline(1,11);
+    addToPipeline(1,12);
+    addToPipeline(1,13);
+    addToPipeline(1,14);
+    addToPipeline(1,15);
+    addToPipeline(1,16);
+    addToPipeline(1,17);
+    addToPipeline(1,18);
+    addToPipeline(1,19);
 
 
 
 
 
-startPipeline();
+    startPipeline();
+
+
 
 
 
